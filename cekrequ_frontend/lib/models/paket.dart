f370786e3a32d98c97900cf84e692e7075068dfc
Amalia181205 <lutvianiamalia@gmail.com>
2026-03-30 19:45:19 +0700

    coba ya, tmbah assets

---
+++ cekrequ_frontend/lib/models/paket.dart
@@ -0,0 +1,15 @@
+class Paket {
+  String nama;
+  String tempat;
+  String warna;
+  int harga;
+  String gambar;
+
+  Paket({
+    required this.nama,
+    required this.tempat,
+    required this.warna,
+    required this.harga,
+    required this.gambar,
+  });
+}
